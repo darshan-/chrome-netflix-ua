cff576e122ae95987da4d15bd6fbc0c45f8c939c
Darshan-Josiah Barber <darshan@darshancomputing.com>
2014-09-23 10:48:31 -0700

    Updated initial code to only alter headers sent to Netflix and to send appropriate UA. (And changed name of extension.

--- background.js
+++ background.js
@@ -2,19 +2,25 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     function(info) {
         // Replace the User-Agent header
         var headers = info.requestHeaders;
+
         headers.forEach(function(header, i) {
-            if (header.name.toLowerCase() == 'user-agent') { 
-                header.value = 'Spoofed UA';
+            if (header.name.toLowerCase() == 'user-agent') {
+                header.value = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2114.2 Safari/537.36';
             }
-        });  
+        });
+
         return {requestHeaders: headers};
     },
     // Request filter
     {
         // Modify the headers for these pages
         urls: [
-            "http://stackoverflow.com/*",
-            "http://127.0.0.1:6789/*"
+            //"http://stackoverflow.com/*",
+            //"http://127.0.0.1:6789/*"
+            //"http://*/*"
+            //"http://www.whatsmyuseragent.com/",
+            //"http://show-ip.net/browserinfo/"
+            "*://*.netflix.com/*"
         ],
         // In the main window and frames
         types: ["main_frame", "sub_frame"]
